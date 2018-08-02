4dd0e2b15937ba7a10759fb7564af23eae1ffbda
Deena Nath <deny.inbox@gmail.com>
2018-08-02 23:47:52 +0530

    Exception Handling

---
+++ ConsoleThoughtWorks/ConsoleThoughtWorks/ExceptionExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleThoughtWorks
+{
+    // you can write multiple catch block for single try block
+    // but you CANT write super class catch first and then derived class catch later, just uncommnet the below code
+    public class ExceptionExample
+    {
+        public void ThrowException()
+        {
+            throw new DivideByZeroException();
+        }
+
+    }
+
+    partial class Program
+    {
+        static void Main7(string[] args) 
+        {
+            ExceptionExample ex = new ExceptionExample();
+            try
+            {
+                ex.ThrowException();
+            }
+            catch (Exception exx)
+            {
+                Console.WriteLine(exx.ToString());
+            }
+            //catch (DivideByZeroException exx)
+            //{
+            //    Console.WriteLine(exx.ToString());
+            //}
+
+
+            Console.ReadLine();
+        }
+    }
+}
