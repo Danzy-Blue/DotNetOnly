daf2d6d20c2f89a225101b49cbd2432218198250
Deena Nath <deny.inbox@gmail.com>
2018-08-02 23:57:53 +0530

    SingletonPattern Approach 1

---
+++ ConsoleThoughtWorks/ConsoleThoughtWorks/SingletonPattern.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleThoughtWorks
+{
+    public class SingletonPattern
+    {
+        private static SingletonPattern obj;
+        private SingletonPattern()
+        {
+            Console.WriteLine($"Constructor Called");
+        }
+
+        public static SingletonPattern getSingletonObject()
+        {
+            if (obj == null)
+            {
+                obj = new SingletonPattern();
+            }
+
+            return obj;
+        }
+    }
+
+    partial class Program
+    {
+        public static void Main()
+        {
+            // 1
+            SingletonPattern.getSingletonObject();
+            SingletonPattern.getSingletonObject();
+            SingletonPattern.getSingletonObject();
+
+            Console.WriteLine();
+        }
+    }
+}
